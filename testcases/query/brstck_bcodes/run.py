bcabd0a4b2d02103ff4af1473efa6acc59cfc320
robertomartinez09515 <robertomartinez09515@gmail.com>
2021-09-08 16:22:49 +0100

    Up batch size

--- testcases/query/brstck_bcodes/run.py
+++ testcases/query/brstck_bcodes/run.py
@@ -11,20 +11,27 @@ class PySysTest(JDSBaseTest):
 	def execute(self):
 		db = self.get_db_connection()
 		db.brstck_bcodes.drop()
+		db.batch_done.delete_many({})
+		db.batch_progress.delete_many({})
+		db.batch_progress.insert_one({'batches_inserted' : 0, 'batches_sent' : 0, 'docs_inserted' : 0, 'docs_sent' : 0})
 		current_ids = []
-		BATCH_SIZE = 1000
+		BATCH_SIZE = 10000
 		cnt = 0
+		batch_index = 0
 		for doc in db.brstck.find({}, {'_id : 1'}, batch_size = BATCH_SIZE):
 			current_ids.append(doc['_id'])
 			# self.log.info(doc['_id'])
 			if len(current_ids) == BATCH_SIZE:
-				pipeline = self.get_pipeline(current_ids)
+				pipeline = self.get_pipeline(current_ids, batch_index)
 				ret = list(db.brstck.aggregate(pipeline, allowDiskUse=True))
 				cnt += len(current_ids)
+				db.batch_progress.update_one({}, { '$inc' : { 'batches_inserted' : 1, 'docs_inserted' : len(current_ids)}})
+				db.batch_done.insert_one({'batch_index' : batch_index})
+				batch_index += 1
 				current_ids = []
 				self.log.info(f'Done {cnt}')
 	
-	def get_pipeline(self, current_ids):
+	def get_pipeline(self, current_ids, batch_index):
 		pipeline = [
 			{
 				'$match': {
@@ -90,10 +97,14 @@ class PySysTest(JDSBaseTest):
 			}, {
 				'$project': {
 					'loc': 1, 
-					'qty': '$qtyATS', 
+					'qty': { '$toString' : '$qtyATS' }, 
 					'sku': 1, 
 					'bcode': '$product.value', 
 					'_id': 0
+				}
+			}, {
+				'$addFields' : {
+					'batch_index' : batch_index
 				}
 			}
 			, {
